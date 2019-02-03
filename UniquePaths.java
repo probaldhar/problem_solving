eb2d8e76118b961a97e3cafe9045241b45e01525
Probal Chandra Dhar <p_dhar@apple.com>
2019-02-03 01:17:37 -0800

    word search.

--- UniquePaths.java
+++ UniquePaths.java
@@ -21,4 +21,60 @@ class UniquePaths {
         
         return matrix[m-1][n-1];
     }
+    
+    /**
+     * 	A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+		The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+		Now consider if some obstacles are added to the grids. How many unique paths would there be?
+		An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+     * @param obstacleGrid
+     * @return
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int row = obstacleGrid.length;
+        int col = obstacleGrid[0].length;
+        
+        // check the first column for obstacle
+        if ( obstacleGrid[0][0] == 1 ) {
+            return 0;
+        }
+        
+        obstacleGrid[0][0] = 1;
+        
+        // obstacle    = 1
+        // empty space = 0
+        
+        // check the first column
+        for ( int i = 1; i < row; i++ ) {
+            if ( obstacleGrid[i][0] == 0 && obstacleGrid[i-1][0] == 1 ) {
+                obstacleGrid[i][0] = 1;
+            } else {
+                obstacleGrid[i][0] = 0;
+            }
+        }
+        
+        // check the first column
+        for ( int i = 1; i < col; i++ ) {
+            if ( obstacleGrid[0][i] == 0 && obstacleGrid[0][i-1] == 1 ) {
+                obstacleGrid[0][i] = 1;
+            } else {
+                obstacleGrid[0][i] = 0;
+            }
+        }
+        
+        for ( int i = 1; i < row; i++ ) {
+            for ( int j = 1; j < col; j++ ) {
+                // 0 means empty space, no obstacle
+                if (obstacleGrid[i][j] == 0) {
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
+                } else {
+                    obstacleGrid[i][j] = 0;
+                }
+            }
+        }
+        
+        return obstacleGrid[row-1][col-1];
+        
+    }
+    
 }
---
+++ WordSearch.java
@@ -0,0 +1,78 @@
+package projects;
+
+/**
+ * 
+ * leetcode link: https://leetcode.com/problems/word-search/
+ * 
+ * @author probaldhar
+ */
+
+class WordSearch {
+    
+    public boolean exist(char[][] board, String word) {
+        // Visited array to keep track of the visited nodes
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        
+        for ( int i = 0; i < board.length; i++ ) {
+            for ( int j = 0; j < board[i].length; j++ ) {
+                
+                // check if the first char matches with the first char of the word
+                // only one dfs call is sufficient as we're checking with backtracking
+                if ( word.charAt(0) == board[i][j] && dfs(board, visited, word, i, j, 0) ) {
+                    return true;
+                }
+                
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean dfs(char[][] board, boolean[][] visited, String word, int row, int col, int index) {
+        // we found the word in the board, yay!!
+        if (index == word.length()) {
+            return true;
+        }
+        
+        if ( !isValid(board, visited, word, row, col, index) ) {
+            return false;
+        }
+        
+        // make the position visited
+        visited[row][col] = true;
+        
+        // straight dfs for the next index
+        if ( dfs(board, visited, word, row - 1, col, index+1) || 
+             dfs(board, visited, word, row, col - 1, index+1) || 
+             dfs(board, visited, word, row + 1, col, index+1) || 
+             dfs(board, visited, word, row, col + 1, index+1) ) {
+            return true;
+        }
+        
+        // BACKTRACK !!
+        // make the visited false to check again from another direction
+        visited[row][col] = false;
+        return false;
+        
+    }
+    
+    public boolean isValid(char[][] board, boolean[][] visited, String word, int row, int col, int index) {
+        if(row < 0 || col < 0 || row >= board.length || col >= board[0].length) {
+            return false;
+        }
+
+        if ( visited[row][col]) {
+            return false;
+        }
+
+        if(board[row][col] != word.charAt(index)) {
+            return false;
+        }
+
+        return true;
+    }
+    
+}
+
+
+
